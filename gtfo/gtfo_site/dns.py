4d26ce614a42e6ae8506327e2047011ca3e8bde1
dan <dan.bate@gmail.com>
2014-05-20 23:14:38 +0100

    Added prefix and suffix

--- gtfo/gtfo_site/dns.py
+++ gtfo/gtfo_site/dns.py
@@ -1,19 +1,19 @@
 import re
-from twisted.names import client
-
-#class Mapping(client.Resolver):
+from twisted.names import client, dns
 from gtfo.gtfo_site import utils
 
-
-class Mapping():
+class Mapping(client.Resolver):
+#class Mapping():
     def __init__(self, servers=None, prefix=None, suffix=None):
-        #super().__init__(servers)
-        if not prefix:
+        super().__init__(servers=servers)
+        if not prefix and not suffix:
             raise ValueError("A hostname prefix or suffix must be supplied")
 
-        self._mapper = re.compile(prefix + "(.+)")
+        self._mapper = re.compile("%s(.+)%s" % (prefix or "", suffix or ""))
 
     def lookupAddress(self, name, timeout=None):
         m = self._mapper.match(name)
         if m:
             return utils.get_server_address() + "/" + m.group(1)
+        else:
+            return self._lookup(name, dns.IN, dns.A, timeout)
--- gtfo/gtfo_site/test/test_Dns_Mapping.py
+++ gtfo/gtfo_site/test/test_Dns_Mapping.py
@@ -9,6 +9,18 @@ class Dns_Mapping(TestCase):
 
     @patch("gtfo.gtfo_site.utils.get_server_address", Mock(return_value="localhost:8000"))
     def test_PrefixIsSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
-        map = dns.Mapping(["ns_server"], prefix="gtfo.")
+        map = dns.Mapping([("ns_server", 8080)], prefix="gtfo.")
 
         self.assertEqual("localhost:8000/foo", map.lookupAddress("gtfo.foo"))
+
+    @patch("gtfo.gtfo_site.utils.get_server_address", Mock(return_value="localhost:8000"))
+    def test_SuffixIsSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
+        map = dns.Mapping([("ns_server", 8080)], suffix=".gtfo")
+
+        self.assertEqual("localhost:8000/foo", map.lookupAddress("foo.gtfo"))
+
+    @patch("gtfo.gtfo_site.utils.get_server_address", Mock(return_value="localhost:8000"))
+    def test_PrefixAndSuffixSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
+        map = dns.Mapping([("ns_server", 8080)], prefix="pre.", suffix=".suf")
+
+        self.assertEqual("localhost:8000/foo", map.lookupAddress("pre.foo.suf"))

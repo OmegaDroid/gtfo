028d5558069409cd1d88b893719bce86d366ba9b
dbate <daniel.bate@irisys.co.uk>
2014-05-21 13:36:59 +0100

    created separate mapping class

--- gtfo/gtfo_site/dns.py
+++ gtfo/gtfo_site/dns.py
@@ -2,18 +2,50 @@ import re
 from twisted.names import client, dns
 from gtfo.gtfo_site import utils
 
-class Mapping(client.Resolver):
-#class Mapping():
-    def __init__(self, servers=None, prefix=None, suffix=None):
-        super().__init__(servers=servers)
+
+class Mapping():
+    def __init__(self, prefix: str=None, suffix: str=None):
+        """
+        Initialises the mapping. The mapped regex is "{{ prefix }}(.+){{ suffix }}". If neither a prefix or suffix are
+        supplied a ValueError is raised
+
+        :param prefix: The prefix for the host name
+        :param suffix: The suffix of the hostname
+        :raises ValueError: If neither a prefix or suffix are supplied
+        """
         if not prefix and not suffix:
             raise ValueError("A hostname prefix or suffix must be supplied")
 
         self._mapper = re.compile("%s(.+)%s" % (prefix or "", suffix or ""))
 
+    def __getitem__(self, key):
+        m = self._mapper.match(key)
+        if not m:
+            raise KeyError
+
+        return utils.get_server_address() + "/" + m.group(1)
+
+    def __contains__(self, key):
+        return self._mapper.match(key)
+
+
+class Resolver(client.Resolver):
+    def __init__(self, mapper: dict(str, str), servers: list(str, int)):
+        """
+        Initialises the Resolver. Mapper needs to behave as a readable dictionary, as a minimum it must implement
+        __contains__ and __getitem__
+
+        :param mapper: The object to control mapping host name to ip
+        :param servers: List of other dns servers to try if the hostname is not present in the map. These should be
+                        hostname, port pairs
+        """
+        self._mapper = mapper
+        super().__init__(self, servers=servers)
+        self.ttl = 10
+
     def lookupAddress(self, name, timeout=None):
-        m = self._mapper.match(name)
-        if m:
-            return utils.get_server_address() + "/" + m.group(1)
+        if name in self._mapper:
+            result = self._mapper[name]
+            return [(dns.RRHeader(name, dns.A, dns.IN, self.ttl, dns.Record_A(result, self.ttl)),), (), ()]
         else:
             return self._lookup(name, dns.IN, dns.A, timeout)
--- gtfo/gtfo_site/test/test_Dns_Mapping.py
+++ gtfo/gtfo_site/test/test_Dns_Mapping.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock
 from django.test import TestCase
+from twisted.names import client
 from gtfo.gtfo_site import dns
 
 
@@ -7,20 +8,45 @@ class Dns_Mapping(TestCase):
     def test_NoPrefixOrSuffixIsSupplied_ValueErrorIsRaised(self):
         self.assertRaises(ValueError, dns.Mapping)
 
+    def test_PrefixIsSuppliedNameMatchesPattern_PatternIsInMap(self):
+        map = dns.Mapping(prefix="gtfo.")
+
+        self.assertIn("gtfo.foo", map)
+
+    def test_SuffixIsSuppliedNameMatchesPattern_PatternIsInMap(self):
+        map = dns.Mapping(suffix=".gtfo")
+
+        self.assertIn("foo.gtfo", map)
+
+    def test_PrefixAndSuffixSuppliedNameMatchesPattern_PatternIsInMap(self):
+        map = dns.Mapping(prefix="pre.", suffix=".suf")
+
+        self.assertIn("pre.foo.suf", map)
+
+    def test_SuppliedNameDoesnotMatchePattern_PatternIsNotInMap(self):
+        map = dns.Mapping(prefix="pre.", suffix=".suf")
+
+        self.assertNotIn("foo", map)
+
     @patch("gtfo.gtfo_site.utils.get_server_address", Mock(return_value="localhost:8000"))
-    def test_PrefixIsSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
-        map = dns.Mapping([("ns_server", 8080)], prefix="gtfo.")
+    def test_PrefixSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
+        map = dns.Mapping(prefix="gtfo.")
 
-        self.assertEqual("localhost:8000/foo", map.lookupAddress("gtfo.foo"))
+        self.assertEqual("localhost:8000/foo", map["gtfo.foo"])
 
     @patch("gtfo.gtfo_site.utils.get_server_address", Mock(return_value="localhost:8000"))
-    def test_SuffixIsSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
-        map = dns.Mapping([("ns_server", 8080)], suffix=".gtfo")
+    def test_SuffixSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
+        map = dns.Mapping(suffix=".gtfo")
 
-        self.assertEqual("localhost:8000/foo", map.lookupAddress("foo.gtfo"))
+        self.assertEqual("localhost:8000/foo", map["foo.gtfo"])
 
     @patch("gtfo.gtfo_site.utils.get_server_address", Mock(return_value="localhost:8000"))
     def test_PrefixAndSuffixSuppliedNameMatchesPattern_TheReturnedStringContainsTheServerHostAndDeviceHost(self):
-        map = dns.Mapping([("ns_server", 8080)], prefix="pre.", suffix=".suf")
+        map = dns.Mapping(prefix="pre.", suffix=".suf")
 
-        self.assertEqual("localhost:8000/foo", map.lookupAddress("pre.foo.suf"))
+        self.assertEqual("localhost:8000/foo", map["pre.foo.suf"])
+
+    def test_SuppliedStringDoesNotMatchThePattern_KeyErrorIsRaised(self):
+        map = dns.Mapping(prefix="pre.", suffix=".suf")
+
+        self.assertRaises(KeyError, map.__getitem__, "foo")

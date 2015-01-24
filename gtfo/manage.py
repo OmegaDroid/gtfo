0f2121288162081c70bee6667e882fc849e55a1a
Daniel Bate <dan@The-Whistler.home>
2015-01-24 18:03:12 +0000

    added project root to path for manage.py

--- gtfo/manage.py
+++ gtfo/manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gtfo_site.settings")
 
